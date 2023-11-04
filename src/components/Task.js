b365e23dea6babef43277f0960bb84a0f23c4a5c
Stanley SkimZ Kimambo <Stanleykimambo@gmail.com>
2023-11-04 09:56:04 +0300

    Task done with a hicup tho

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Task() {
+function Task({ text, category, index }) {
+  // console.log(category)
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleDelete = () => {
+    setIsVisible(false)
+    if(!isVisible) {
+      console.log(isVisible)
+    } else {
+      console.log('What did you think1, its ', isVisible)
+    }
+    // const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    // setTasks(updatedTasks);
+  };
+
+  console.log(isVisible);
   return (
-    <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
-    </div>
+    <>
+      {isVisible && (
+        <div className="task" key={index+1}>
+          <div className="label" key={index+2} >
+            <select>
+            <option value={category} key={category}>{category}</option>
+            {/* <option value={category[1]}>{category[1]}</option>
+            <option value={category[2]}>{category[2]}</option>
+            <option value={category[3]}>{category[3]}</option>
+            <option value={category[4]}>{category[4]}</option> */}
+            {/* <option value={category}>{category}</option> */}
+              {/* <option value={categories[0]}>{categories[0]}</option> */}
+            </select>
+          </div>
+
+          <div className="text" key={index}>{text}</div>
+
+          <button className="delete" onClick={handleDelete} key={index+3} >
+            X
+          </button>
+        </div>
+      )}
+    </>
   );
 }
 
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Task from "./Task";
+import { TASKS, CATEGORIES } from "../data";
 
 function TaskList() {
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+
+      {TASKS.map((task, index) => (<Task text={task.text} category={CATEGORIES} index={index} />))}
+      
     </div>
   );
 }
